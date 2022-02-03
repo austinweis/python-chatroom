f93eec6e562a9db325fee96e76f682f25d5ecd01
austinweis <austinweis@protonmail.com>
2022-02-03 01:50:50 -0700

    async

--- src/server.py
+++ src/server.py
@@ -1,20 +1,41 @@
-import socket, selectors
+#!/usr/bin/env python3
 
-HOST = "127.0.0.1"
-PORT = 1234
+import socket, asyncio
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print("starting server...")
-    s.bind((HOST, PORT))
-    print("binding socket to host...")
-    s.listen()
-    print("listening on port...\n")
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            conn.sendall(b"message received")
-            if data.decode("UTF-8") == "close":
-                print("closing...")
-                break
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 1234        # Port to listen on (non-privileged ports are > 1023)
+
+connections = []
+
+async def receive_data(conn):
+    return conn.recv(1024)
+async def client_handler():
+    print("handle")
+    for conn, addr in connections:
+        with conn:
+            print('Connected by', addr)
+            while True:
+                print("loop")
+                data = await receive_data(conn)
+                print(data)
+                if not data:
+                    break
+                await conn.sendall(data)
+
+async def client_listener():
+    while True:
+        print("listen")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            await connections.append(s.accept())
+
+async def main():
+    listen = asyncio.create_task(client_listener())
+    handle = asyncio.create_task(client_handler())
+
+    await listen
+    await handle
+
+
+asyncio.run(main())
