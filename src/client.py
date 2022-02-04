aab041cd5efbfbba7be1d34247d1cc7def99b52f
austinweis <austinweis@protonmail.com>
2022-02-04 07:47:46 -0700

    something

--- src/client.py
+++ src/client.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = "127.0.0.1"
-PORT = 1234
+PORT = 8888
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- src/server.py
+++ src/server.py
@@ -1,41 +1,26 @@
 #!/usr/bin/env python3
+import asyncio
 
-import socket, asyncio
-
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 1234        # Port to listen on (non-privileged ports are > 1023)
-
-connections = []
-
-async def receive_data(conn):
-    return conn.recv(1024)
-async def client_handler():
-    print("handle")
-    for conn, addr in connections:
-        with conn:
-            print('Connected by', addr)
-            while True:
-                print("loop")
-                data = await receive_data(conn)
-                print(data)
-                if not data:
-                    break
-                await conn.sendall(data)
-
-async def client_listener():
+async def handle_client(reader, writer):
     while True:
-        print("listen")
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            await connections.append(s.accept())
+        data = await reader.read(100)
+        message = data.decode()
+        addr = writer.get_extra_info('peername')
+
+        print(f"Received {message!r} from {addr!r}")
+
+        print(f"Send: {message!r}")
+        writer.write(data)
+        await writer.drain()
 
 async def main():
-    listen = asyncio.create_task(client_listener())
-    handle = asyncio.create_task(client_handler())
+    server = await asyncio.start_server(
+        handle_client, '127.0.0.1', 8888)
 
-    await listen
-    await handle
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Serving on {addrs}')
 
+    async with server:
+        await server.serve_forever()
 
 asyncio.run(main())
